054bef2c8b377de5fad9046134687867ad57745c
riad40 <riadabdelaziz40@gmail.com>
2023-05-09 13:57:37 +0100

    set up initial files for the app styling

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native"
+import { screenBackground } from "../../constants/colors"
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: screenBackground,
+    },
+})
+
+export default styles
