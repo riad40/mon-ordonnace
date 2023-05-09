a03ea62d8b214413dff3eedf80fffb4988de52a0
riad40 <riadabdelaziz40@gmail.com>
2023-05-09 14:03:41 +0100

    start working on the NavBar component

---
+++ src/components/common/NavBar.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+const NavBar = (): JSX.Element => {
+    return (
+        <View>
+            <Text>NavBar</Text>
+        </View>
+    )
+}
+
+export default NavBar
