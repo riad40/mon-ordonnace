40c3f7547db0a3f132e30249b3543feb598068bf
riad40 <riadabdelaziz40@gmail.com>
2023-05-10 17:05:39 +0100

    Update the NavBar component - change the logo to the back button when the user is in a nested route

--- src/components/common/navbar/NavBar.tsx
+++ src/components/common/navbar/NavBar.tsx
@@ -1,13 +1,24 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, TouchableOpacity } from "react-native"
 import navStyles from "../../../assets/styles/navbar"
 import ProfileIcon from "./ProfileIcon"
+import { NavigationProp } from "@react-navigation/native"
 
-const NavBar = (): JSX.Element => {
+const NavBar = ({ navigation }: { navigation?: NavigationProp<any> }): JSX.Element => {
     const boldTitle = "MON"
     const lightTitle = "ORDONNANCE"
     return (
         <View style={navStyles.container}>
-            <Image source={require("../../../assets/images/Headset.png")} />
+            {navigation ? (
+                <TouchableOpacity style={navStyles.backContainer}>
+                    <Image source={require("../../../assets/images/back.png")} />
+                    <Text style={navStyles.backText} onPress={() => navigation.goBack()}>
+                        retour
+                    </Text>
+                </TouchableOpacity>
+            ) : (
+                <Image source={require("../../../assets/images/Headset.png")} />
+            )}
+
             <View style={navStyles.titleContainer}>
                 <Text style={navStyles.boldTitle}>{boldTitle}</Text>
                 <Text style={navStyles.lightTitle}>{lightTitle}</Text>
