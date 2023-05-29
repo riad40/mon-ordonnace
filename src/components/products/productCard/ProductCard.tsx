71f08aa07fbd325da16e593439fc92c49b421545
riad40 <riadabdelaziz40@gmail.com>
2023-05-29 16:52:26 +0100

    update the api url

--- src/components/products/productCard/ProductCard.tsx
+++ src/components/products/productCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Image, Text, Pressable, View } from "react-native"
 import { Product } from "../../../@types/"
 import productCardStyles from "./productCardStyles"
+import API_URL from "../../../configs/API_URL"
 
 const ProductCard = ({ product, onPress }: { product: Product; onPress?: () => void }) => {
     return (
@@ -8,15 +9,15 @@ const ProductCard = ({ product, onPress }: { product: Product; onPress?: () => v
             <View style={productCardStyles.card}>
                 <View>
                     <View style={productCardStyles.cardContent}>
-                        <Image source={{ uri: product.image }} style={productCardStyles.image} />
+                        <Image source={{ uri: API_URL + product.avatar }} style={productCardStyles.image} />
                         <View style={productCardStyles.details}>
                             <Text style={productCardStyles.name}>{product.name}</Text>
-                            <Text style={productCardStyles.type}>{product.type}</Text>
+                            <Text style={productCardStyles.type}>{product.dci}</Text>
                         </View>
                     </View>
                     <View style={productCardStyles.therapeuticClass}>
                         <Image source={require("../../../assets/images/Location.png")} style={{ marginRight: 5 }} />
-                        <Text style={productCardStyles.type}>{product.therapeuticClass}</Text>
+                        <Text style={productCardStyles.type}>{product.classTherapeutic}</Text>
                     </View>
                 </View>
                 <Image source={require("../../../assets/images/More.png")} />
---
+++ src/configs/API_URL.ts
@@ -0,0 +1,3 @@
+const API_URL = "https://mon-ordonnance-api-1.onrender.com/"
+
+export default API_URL
