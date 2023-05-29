4d8619af03869ff3ff998e60c9774c56810efb6e
riad40 <riadabdelaziz40@gmail.com>
2023-05-29 17:28:38 +0100

    added calculateAge helper

---
+++ src/helpers/calculateAge.ts
@@ -0,0 +1,15 @@
+const calculateAge = (dateString: string): number => {
+    const [day, month, year] = dateString.split("-").map(Number)
+    const today = new Date()
+    const birthDate = new Date(year, month - 1, day)
+    let age = today.getFullYear() - birthDate.getFullYear()
+    const monthDiff = today.getMonth() - birthDate.getMonth()
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--
+    }
+
+    return age
+}
+
+export default calculateAge
