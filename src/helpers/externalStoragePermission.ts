44ddf5a77c5fa46c5df3684dc514015a3f382e9a
riad40 <riadabdelaziz40@gmail.com>
2023-05-30 22:38:29 +0100

    created request for permission to access external storage on android

---
+++ src/helpers/externalStoragePermission.ts
@@ -0,0 +1,28 @@
+import { PermissionsAndroid } from "react-native"
+
+const requestExternalStoragePermission = async () => {
+    let isGranted: boolean = false
+
+    try {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
+            title: "External Storage Permission",
+            message: "App needs access to your external storage " + "so you can save your prescriptions.",
+            buttonNeutral: "Ask Me Later",
+            buttonNegative: "Cancel",
+            buttonPositive: "OK",
+        })
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("You can use the external storage")
+            isGranted = true
+        } else {
+            console.log("External storage permission denied")
+            isGranted = false
+        }
+    } catch (err) {
+        console.warn(err)
+    }
+
+    return isGranted
+}
+
+export default requestExternalStoragePermission
