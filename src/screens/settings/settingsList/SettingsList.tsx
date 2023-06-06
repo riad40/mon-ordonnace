fcfc4d27007137313dfb08d588ed35ec0fb43833
riad40 <riadabdelaziz40@gmail.com>
2023-06-06 18:28:41 +0100

    made the name dynamique

--- src/screens/settings/settingsList/SettingsList.tsx
+++ src/screens/settings/settingsList/SettingsList.tsx
@@ -4,18 +4,18 @@ import { SettingsStackNavProps } from "../../../navigation/stacks/settingsStack/
 import settingsListStyles from "./settingsListStyles"
 import styles from "../../../assets/styles"
 import { heightPercentageToDP as hp } from "react-native-responsive-screen"
+import { RootState } from "../../../state/store"
+import { useAppSelector } from "../../../state/hooks"
 
-const SettingsList = ({
-    navigation,
-}: {
-    navigation: SettingsStackNavProps<"SettingsList">["navigation"]
-}): JSX.Element => {
+const SettingsList = ({ navigation }: { navigation: SettingsStackNavProps<"SettingsList">["navigation"] }): JSX.Element => {
+    const { user } = useAppSelector((state: RootState) => state.profile)
+
     return (
         <SafeAreaView>
             <ScrollView style={styles.appContainer}>
                 <NavBar />
                 <View style={settingsListStyles.container}>
-                    <Heading text="Abderazzaq Hakimi" />
+                    <Heading text={`${user?.fullName}`} />
                 </View>
                 <SimpleCard text="Profile" onPress={() => navigation.navigate("Profile")} />
                 <SimpleCard text="Changer mon compte" />
