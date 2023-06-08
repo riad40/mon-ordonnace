9987b886696aa55ff8c1b19d6e64307b5ec05a1c
riad40 <riadabdelaziz40@gmail.com>
2023-06-08 05:12:05 +0100

    added disabled prop

--- src/components/common/buttons/wideButton/WideButton.tsx
+++ src/components/common/buttons/wideButton/WideButton.tsx
@@ -1,9 +1,19 @@
 import { Text, TouchableOpacity } from "react-native"
 import wideButtonStyles from "./wideButtonStyles"
 
-const WideButton = ({ text, onPress, ref }: { text: string; onPress?: () => void; ref?: React.RefObject<TouchableOpacity> }): JSX.Element => {
+const WideButton = ({
+    text,
+    onPress,
+    ref,
+    disabled,
+}: {
+    text: string
+    onPress?: () => void
+    ref?: React.RefObject<TouchableOpacity>
+    disabled?: boolean
+}): JSX.Element => {
     return (
-        <TouchableOpacity style={wideButtonStyles.button} onPress={onPress} ref={ref}>
+        <TouchableOpacity style={wideButtonStyles.button} onPress={onPress} ref={ref} disabled={disabled}>
             <Text style={wideButtonStyles.buttonText}>{text}</Text>
         </TouchableOpacity>
     )
